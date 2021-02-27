37403f3ff6bfce13ba0d76b3a11f78693809c734
Lucas Cunha <lucasviana112@gmail.com>
2021-02-27 09:32:13 -0300

    database connection and refactor server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
+import router from './routes'
 
 const app = express();
 
-app.get('/user', (request, response) => {
-    return response.json({ message:'ok' });
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running...'));
